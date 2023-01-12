28a10c0e8aa8d973b5c8ba04b5c21244cf2e383c
fracescoMain <cesaranofrancescomain@gmail.com>
2023-01-12 17:13:47 +0100

    aggiunto collegamento alla chiamata ma nonfunziona

--- back-end/data.php
+++ back-end/data.php
@@ -2,7 +2,7 @@
 
 
 
-
+//cre un database su un server
 $todoList = [
     [
         "text" => "todo 1",
@@ -21,9 +21,20 @@ $todoList = [
         "completed" => false
     ]
 ];
-
+//do i permessi al mio front end
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Headers: X-Requested-With");
+//dico alla macchina che si tratta di un database json
 header('Content-Type: application/json');
 
+//prendo dal front end la variabile newtodo
+$newTodo = $_GET['newTodo'];
+
+//la pusho nel db
+$todoList[] = [
+    "text" => $newTodo,
+    "completed" => false
+];
+
+//codifico i dati ni mood che siano leggibli
 echo json_encode($todoList);

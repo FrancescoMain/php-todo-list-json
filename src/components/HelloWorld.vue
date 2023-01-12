e63223bf4b8bb5956281ae6e67dd8ed355b89e75
fracescoMain <cesaranofrancescomain@gmail.com>
2023-01-12 16:12:03 +0100

    strutura di base statica

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,23 @@
-<script setup>
-import { ref } from 'vue'
+<script >
 
-defineProps({
-  msg: String,
-})
 
-const count = ref(0)
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+ <h1>to do List</h1>
+ <ul>
+  <li>cosa</li>
+  <li>cosa</li>
+  <li>cosa</li>
+ </ul>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+ <form>
+  <input type="text">
+  <input type="submit" value="sumbit">
+ </form>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>

5c8fc7076301fa9c261abfaeccc52c0ab9baebab
francescoMain <cesaranofrancescomain@gmail.com>
2023-01-14 11:45:47 +0100

    aggiunta funzione delete item

--- back-end/delet.php
+++ back-end/delet.php
@@ -12,8 +12,13 @@ $index = $_GET['index'];
 $jsonTodoList = file_get_contents("todo.json");
 $todoList = json_decode($jsonTodoList);
 
-$todoList[$index] = [];
 
+function delete_item($todoList,$index){
+   unset($todoList[$index]);
+    return array_merge($todoList);
+}
+
+$todoList = delete_item($todoList,$index);
 $jsonTodoList = json_encode($todoList);
 file_put_contents('todo.json' , $jsonTodoList);
 
